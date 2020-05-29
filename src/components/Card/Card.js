3823ffda5e906998cbac001430f9c5061197327d
lokeam <lokeahnming@gmail.com>
2020-05-29 17:51:15 -0400

    feat: test product item mapping of sample response, wired up Card component to receive props from Grid

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,18 +5,26 @@ import './card-styles.scss';
 class Card extends PureComponent {
   render() {
 
+    const { item: { msrp, price, product, soldOut } } = this.props;
+    const { item } = this.props;
+
+    const productStatus = soldOut ? 'card__status--soldout' : '';
+    const pricePlusDecimal = price.toFixed(2);
+    const msrpPlusDecimal = msrp.toFixed(2);
+
+    console.log("Card, testing item: ", item);
     return(
-      <div className="card">
+      <div className={`card ${productStatus}`}>
         <div className="card__product-msg">
-          <div className="card__logo--soldout"></div>
+          { soldOut && ( <div className="card__logo--soldout">sold out</div> ) }
         </div>
         <div className="card__thumbnail">
-          <img className="card__svg-placeholder" src={produceIcon} alt="placeholder produce icon"/>
+          <img className="card__svg-placeholder" src={produceIcon} alt="placeholder produce icon" />
         </div>
         <div className="card__text">
-          <div className="card__heading">heading here</div>
-          <div className="card__msrp">market retail price</div>
-          <div className="card__pricelabel">price</div>
+          <div className="card__heading">{product}</div>
+          <div className="card__msrp">MSRP: <span className="card__msrp-dec">{msrpPlusDecimal}</span></div>
+          <div className="card__pricelabel">Price: <span className="card__msrp-dec">{pricePlusDecimal}</span></div>
         </div>
       </div>
     );
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,14 +1,20 @@
 import React, { PureComponent } from 'react';
 import Card from '../Card/Card';
 
+/* debug */
+import sample from '../../static/responses/grid-sample.json';
+
 import './grid-styles.scss';
 
 class Grid extends PureComponent {
 
   render() {
+    console.log('testing sample: ', sample);
     return(
       <div className="grid">
-        <Card />
+        { sample.data.items.map( (item) => (
+          <Card key={`gridCard--${item.product}`} item={item} {...item}/>
+        ) ) }
       </div>
     );
   }
