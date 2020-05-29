0ac3fe542dda775f0176763bb43bc4587e1b2c4e
lokeam <lokeahnming@gmail.com>
2020-05-29 15:53:23 -0400

    feat: set up application directory architecture + base sass includes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">I'm the nav. Woot.</nav>
+      <div className="masthead">
+        <h1 className="masthead__heading">I'm the Masthead.</h1>
+      </div>
+      <div className="grid">
+        <p>I'm the grid.</p>
+        <div className="card">
+          <p>I'm a card. Huzzah.</p>
+        </div>
+      </div>
     </div>
   );
 }
